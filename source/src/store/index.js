e46f89806f5714e4e9848ac3b827dc2b0df0e9dd
Aucdmn <auop129@163.com>
2020-08-29 16:00:40 +0800

    vuex -- finish

--- source/src/store/index.js
+++ source/src/store/index.js
@@ -3,9 +3,18 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+let defaultCity = '贵阳'
+try {
+  if (localStorage.city) {
+    defaultCity = localStorage.city
+  }
+} catch (error) {
+
+}
+
 export default new Vuex.Store({
   state: {
-    city: '贵阳'
+    city: defaultCity
   },
   actions: {
     changeCity (ctx, city) {
@@ -15,6 +24,10 @@ export default new Vuex.Store({
   mutations: {
     changeCity (state, city) {
       state.city = city
+      try {
+        localStorage.city = city
+      } catch (error) {
+      }
     }
   }
 })
