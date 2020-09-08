26efcfa74dd9ebc9b353086967555b4310d490df
Aucdmn <auop129@163.com>
2020-09-08 10:33:44 +0800

    Add animation to banner

--- source/src/pages/detail/components/Banner.vue
+++ source/src/pages/detail/components/Banner.vue
@@ -15,16 +15,19 @@
         </div>
       </div>
     </div>
-    <common-gallery
-      :imgs="bannerImgs"
-      v-show="showGallery"
-      @close="handleGalleryClose"
+    <fade-animation>
+      <common-gallery
+        :imgs="bannerImgs"
+        v-show="showGallery"
+        @close="handleGalleryClose"
       ></common-gallery>
+    </fade-animation>
   </div>
 </template>
 
 <script>
 import CommonGallery from 'common/gallery/Gallery'
+import FadeAnimation from 'common/fade/FadeAnimation'
 export default {
   name: 'DetailBanner',
   props: {
@@ -46,7 +49,8 @@ export default {
     }
   },
   components: {
-    CommonGallery
+    CommonGallery,
+    FadeAnimation
   }
 }
 </script>
